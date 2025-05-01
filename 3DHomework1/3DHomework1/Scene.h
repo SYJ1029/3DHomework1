9dd567376284b441437df84cf728a7ecea1be17a
DESKTOP-HGMKATH\abg09 <abg0986@gmail.com>
2025-05-01 23:58:12 +0900

    메인화면을 결정하는 CStartScene을 선언하자 StartScene은 Collision을 할 필요가 없으므로 자식 클래스에서 빼주자

--- 3DHomework1/3DHomework1/Scene.h
+++ 3DHomework1/3DHomework1/Scene.h
@@ -40,13 +40,30 @@ public:
 
 };
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-class CStartScene
+class CStartScene : public CBaseScene
 {
+public:
+	CStartScene();
+	virtual ~CStartScene();
+private:
+	CGameObject** m_ppObjects = NULL;
 
+public:
+	virtual void BuildObjects();
+	virtual void ReleaseObjects();
+
+	virtual void Animate(float fElapsedTime);
+	virtual void Render(HDC hDCFrameBuffer, CCamera* pCamera);
+
+	virtual void OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+	virtual void OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+
+	CGameObject* PickObjectPointedByCursor(int xClient, int yClient, CCamera* pCamera);
 };
 
-////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 class CTankScene : public CBaseScene
