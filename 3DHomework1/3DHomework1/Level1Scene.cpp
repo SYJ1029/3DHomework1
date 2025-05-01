2216e86ec107b26841acc6bf06d0f9f097cfee63
DESKTOP-HGMKATH\abg09 <abg0986@gmail.com>
2025-05-02 00:13:38 +0900

    Level1Scene(아직 아무런 기능도 하지 않음)으로 그냥 전환해보니 그건 된다

--- 3DHomework1/3DHomework1/Level1Scene.cpp
+++ 3DHomework1/3DHomework1/Level1Scene.cpp
@@ -1,3 +1,68 @@
 #include "stdafx.h"
 #include "Scene.h"
 #include "GraphicsPipeline.h"
+
+
+CLevel1Scene::CLevel1Scene()
+{
+
+}
+CLevel1Scene::~CLevel1Scene()
+{
+
+}
+
+void CLevel1Scene::BuildObjects()
+{
+
+}
+void CLevel1Scene::ReleaseObjects()
+{
+
+}
+
+void CLevel1Scene::CheckObjectByObjectCollisions()
+{
+
+}
+void CLevel1Scene::CheckObjectByWallCollisions()
+{
+
+}
+void CLevel1Scene::CheckPlayerByWallCollision()
+{
+
+}
+void CLevel1Scene::CheckObjectByBulletCollisions()
+{
+
+}
+
+void CLevel1Scene::SetPlayer(CPlayer* pPlayer)
+{
+	m_pPlayer = pPlayer;
+}
+
+void CLevel1Scene::Animate(float fElapsedTime)
+{
+
+}
+void CLevel1Scene::Render(HDC hDCFrameBuffer, CCamera* pCamera)
+{
+
+}
+
+void CLevel1Scene::OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam)
+{
+
+}
+void CLevel1Scene::OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam)
+{
+
+}
+
+CGameObject* CLevel1Scene::PickObjectPointedByCursor(int xClient, int yClient, CCamera* pCamera)
+{
+
+	return NULL;
+}
