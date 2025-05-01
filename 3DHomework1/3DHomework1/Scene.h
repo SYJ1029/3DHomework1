c4da5188e4ed18135d8d9b54d6e6b4392df36c45
DESKTOP-HGMKATH\abg09 <abg0986@gmail.com>
2025-05-02 00:02:48 +0900

    나머지 역할을 구현할 자식 Scene들도 선언해주자

--- 3DHomework1/3DHomework1/Scene.h
+++ 3DHomework1/3DHomework1/Scene.h
@@ -65,6 +65,69 @@ public:
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+class CMenuScene : public CBaseScene
+{
+public:
+	CMenuScene();
+	virtual ~CMenuScene();
+private:
+	CGameObject** m_ppObjects = NULL;
+
+public:
+	virtual void BuildObjects();
+	virtual void ReleaseObjects();
+
+	virtual void Animate(float fElapsedTime);
+	virtual void Render(HDC hDCFrameBuffer, CCamera* pCamera);
+
+	virtual void OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+	virtual void OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+
+	CGameObject* PickObjectPointedByCursor(int xClient, int yClient, CCamera* pCamera);
+};
+
+/// //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class CLevel1Scene : public CBaseScene
+{
+public:
+	CLevel1Scene();
+	virtual ~CLevel1Scene();
+private:
+	int							m_nObjects = 0;
+	CGameObject** m_ppObjects = NULL;
+
+	CWallsObject* m_pWallsObject = NULL;
+
+	CPlayer* m_pPlayer = NULL;
+
+#ifdef _WITH_DRAW_AXIS
+	CGameObject* m_pWorldAxis = NULL;
+#endif
+
+public:
+	virtual void BuildObjects();
+	virtual void ReleaseObjects();
+
+	void CheckObjectByObjectCollisions();
+	void CheckObjectByWallCollisions();
+	void CheckPlayerByWallCollision();
+	void CheckObjectByBulletCollisions();
+
+	virtual void SetPlayer(CPlayer* pPlayer);
+
+	virtual void Animate(float fElapsedTime);
+	virtual void Render(HDC hDCFrameBuffer, CCamera* pCamera);
+
+	virtual void OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+	virtual void OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
+
+	CGameObject* PickObjectPointedByCursor(int xClient, int yClient, CCamera* pCamera);
+};
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 class CTankScene : public CBaseScene
 {
