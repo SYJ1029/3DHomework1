cde07f73a002eb9e384f13b1fd54fd1c2478b6d0
SYJ1029 <abg0986@gmail.com>
2025-06-04 19:03:17 +0900

    내가 사용했던 직선의 육면체 확장법은 직선만큼 육면체가 필요하지만
    내 기존 코드들은 생성될 육면체들을 모아서 한번에 올려야 하는 방식이다

--- 3DHomework1/3DHomework2/FMeshBuilder.cpp
+++ 3DHomework1/3DHomework2/FMeshBuilder.cpp
@@ -15,7 +15,17 @@ CMeshBuilder::CMeshBuilder(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* 
 
 	: CMesh(pd3dDevice,	pd3dCommandList)
 {
+	
+	float fHalfWidth = fWidth / 2;
+	float fHalfHeight = fHeight / 8;
+	float fHalfDepth = fDepth / 2;
 
+	Loadlines(filename, lines);
+
+	//m_nPolygons = lines.size() * 6;
+	//m_ppPolygons = new CPolygon * [m_nPolygons];
+
+	LinesToCube(pd3dDevice, pd3dCommandList, lines);
 }
 CMeshBuilder::~CMeshBuilder()
 {
@@ -54,8 +64,67 @@ void CMeshBuilder::SingleLineToCube(ID3D12Device* pd3dDevice, ID3D12GraphicsComm
 	XMFLOAT3 p6{ p2.x, p2.y, p2.z + depth };
 	XMFLOAT3 p7{ p3.x, p3.y, p3.z + depth };
 
-	auto topFace = { p1, p5, p6, p2 };
-	auto bottomFace = { p0, p4, p7, p3 };
+	//auto topFace = { p1, p5, p6, p2 };
+	//auto bottomFace = { p0, p4, p7, p3 };
+
+
+	//직육면체는 꼭지점(정점)이 8개이다. 
+	m_nVertices = 8;
+	m_nStride = sizeof(CDiffusedVertex);
+	m_d3dPrimitiveTopology = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+	//정점 버퍼는 직육면체의 꼭지점 8개에 대한 정점 데이터를 가진다. 
+	CDiffusedVertex pVertices[8];
+	pVertices[0] = CDiffusedVertex(p1, RANDOM_COLOR);
+	pVertices[1] = CDiffusedVertex(p5, RANDOM_COLOR);
+	pVertices[2] = CDiffusedVertex(p6, RANDOM_COLOR);
+	pVertices[3] = CDiffusedVertex(p2, RANDOM_COLOR);
+	pVertices[4] = CDiffusedVertex(p0, RANDOM_COLOR);
+	pVertices[5] = CDiffusedVertex(p4, RANDOM_COLOR);
+	pVertices[6] = CDiffusedVertex(p7, RANDOM_COLOR);
+	pVertices[7] = CDiffusedVertex(p3, RANDOM_COLOR);
+	m_pd3dVertexBuffer = ::CreateBufferResource(pd3dDevice, pd3dCommandList, pVertices,
+		m_nStride * m_nVertices, D3D12_HEAP_TYPE_DEFAULT,
+		D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER, &m_pd3dVertexUploadBuffer);
+	m_d3dVertexBufferView.BufferLocation = m_pd3dVertexBuffer->GetGPUVirtualAddress();
+	m_d3dVertexBufferView.StrideInBytes = m_nStride;
+	m_d3dVertexBufferView.SizeInBytes = m_nStride * m_nVertices;
+	/*인덱스 버퍼는 직육면체의 6개의 면(사각형)에 대한 기하 정보를 갖는다. 삼각형 리스트로 직육면체를 표현할 것이
+	므로 각 면은 2개의 삼각형을 가지고 각 삼각형은 3개의 정점이 필요하다. 즉, 인덱스 버퍼는 전체 36(=6*2*3)개의 인
+	덱스를 가져야 한다.*/
+	m_nIndices = 36;
+	UINT pnIndices[36];
+	//ⓐ 앞면(Front) 사각형의 위쪽 삼각형
+	pnIndices[0] = 3; pnIndices[1] = 1; pnIndices[2] = 0;
+	//ⓑ 앞면(Front) 사각형의 아래쪽 삼각형
+	pnIndices[3] = 2; pnIndices[4] = 1; pnIndices[5] = 3;
+	//ⓒ 윗면(Top) 사각형의 위쪽 삼각형
+	pnIndices[6] = 0; pnIndices[7] = 5; pnIndices[8] = 4;
+	//ⓓ 윗면(Top) 사각형의 아래쪽 삼각형
+	pnIndices[9] = 1; pnIndices[10] = 5; pnIndices[11] = 0;
+	//ⓔ 뒷면(Back) 사각형의 위쪽 삼각형
+	pnIndices[12] = 3; pnIndices[13] = 4; pnIndices[14] = 7;
+	//ⓕ 뒷면(Back) 사각형의 아래쪽 삼각형
+	pnIndices[15] = 0; pnIndices[16] = 4; pnIndices[17] = 3;
+	//ⓖ 아래면(Bottom) 사각형의 위쪽 삼각형
+	pnIndices[18] = 1; pnIndices[19] = 6; pnIndices[20] = 5;
+	//ⓗ 아래면(Bottom) 사각형의 아래쪽 삼각형
+	pnIndices[21] = 2; pnIndices[22] = 6; pnIndices[23] = 1;
+	//ⓘ 옆면(Left) 사각형의 위쪽 삼각형
+	pnIndices[24] = 2; pnIndices[25] = 7; pnIndices[26] = 6;
+	//ⓙ 옆면(Left) 사각형의 아래쪽 삼각형
+	pnIndices[27] = 3; pnIndices[28] = 7; pnIndices[29] = 2;
+	//ⓚ 옆면(Right) 사각형의 위쪽 삼각형
+	pnIndices[30] = 6; pnIndices[31] = 4; pnIndices[32] = 5;
+	//ⓛ 옆면(Right) 사각형의 아래쪽 삼각형
+	pnIndices[33] = 7; pnIndices[34] = 4; pnIndices[35] = 6;
+	//인덱스 버퍼를 생성한다. 
+	m_pd3dIndexBuffer = ::CreateBufferResource(pd3dDevice, pd3dCommandList, pnIndices,
+		sizeof(UINT) * m_nIndices, D3D12_HEAP_TYPE_DEFAULT, D3D12_RESOURCE_STATE_INDEX_BUFFER,
+		&m_pd3dIndexUploadBuffer);
+	//인덱스 버퍼 뷰를 생성한다. 
+	m_d3dIndexBufferView.BufferLocation = m_pd3dIndexBuffer->GetGPUVirtualAddress();
+	m_d3dIndexBufferView.Format = DXGI_FORMAT_R32_UINT;
+	m_d3dIndexBufferView.SizeInBytes = sizeof(UINT) * m_nIndices;
 }
 
 void CMeshBuilder::LinesToCube(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList
--- 3DHomework1/3DHomework2/TitleShader.cpp
+++ 3DHomework1/3DHomework2/TitleShader.cpp
@@ -13,7 +13,7 @@ CTitleShader::~CTitleShader()
 void CTitleShader::BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList
 	* pd3dCommandList)
 {
-	m_nObjects = 9;
+	m_nObjects = 1;
 	m_ppObjects = new CGameObject * [m_nObjects];
 	float fxPitch = 12.0f * 2.5f;
 	float fyPitch = 12.0f * 2.5f;
@@ -23,13 +23,17 @@ void CTitleShader::BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandL
 		pExplosiveObject = new CExplosiveObject();
 		pExplosiveObject->SetPosition(fxPitch * i, 0.0f, fzPitch * i);
 		pExplosiveObject->SetRotationAxis(XMFLOAT3(0.0f, 1.0f, 0.0f));
-		pExplosiveObject->SetRotationSpeed(10.0f * (i % 10));
+		pExplosiveObject->SetRotationSpeed(30.0f);
 		m_ppObjects[i] = pExplosiveObject;
 	}
+
+	std::list<std::pair<CDiffusedVertex*, CDiffusedVertex*>> lines;
+
 	//인스턴싱을 사용하여 렌더링하기 위하여 하나의 게임 객체만 메쉬를 가진다.
-	CTankMeshDiffused* pTankMesh = new CTankMeshDiffused(pd3dDevice, pd3dCommandList,
+	CMeshBuilder* pNameMesh = new CMeshBuilder(pd3dDevice, pd3dCommandList,
+		"3D게임 프로그래밍.txt", lines,
 		12.0f, 3.0f, 12.0f);
-	m_ppObjects[0]->SetMesh(pTankMesh);
+	m_ppObjects[0]->SetMesh(pNameMesh);
 
 	//인스턴싱을 위한 버퍼(Structured Buffer)를 생성한다.
 	CreateShaderVariables(pd3dDevice, pd3dCommandList);
